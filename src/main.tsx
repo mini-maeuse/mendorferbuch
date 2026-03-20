43244958a5990b137577930bc2f5e4e5805bba0e
Patrick-Jaeger <45011971+Patrick-Jaeger@users.noreply.github.com>
2026-03-20 11:34:55 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom"; // hinzufügen
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <App />
+  </BrowserRouter>
+);
